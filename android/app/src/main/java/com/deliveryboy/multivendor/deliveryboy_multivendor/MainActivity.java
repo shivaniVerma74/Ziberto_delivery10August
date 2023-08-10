696111376a352f000594e71bba3839263303d1f9
Shivani Verma <shivanialphawizz@gmail.com>
2023-08-10 18:18:31 +0530

    first commit

---
+++ android/app/src/main/java/com/deliveryboy/multivendor/deliveryboy_multivendor/MainActivity.java
@@ -0,0 +1,6 @@
+package com.deliveryboy.multivendor;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Screens/Splash/splash.dart
@@ -0,0 +1,109 @@
+import 'dart:async';
+import 'package:deliveryboy_multivendor/Helper/Session.dart';
+import 'package:deliveryboy_multivendor/Screens/Authentication/login.dart';
+import 'package:deliveryboy_multivendor/Screens/home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../../Helper/color.dart';
+import '../../Helper/string.dart';
+
+//splash screen of app
+class Splash extends StatefulWidget {
+  @override
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<Splash> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.top]);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ));
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    deviceHeight = MediaQuery.of(context).size.height;
+    deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            'assets/images/doodle.png',
+            fit: BoxFit.fill,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color:Colors.transparent,
+            child: Center(
+              child: Image.asset(
+                'assets/images/splashlogo.png',
+                height: 150,
+                width: 150,
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+
+  startTime() async {
+    var _duration = Duration(seconds: 2);
+    return Timer(_duration, navigationPage);
+  }
+
+  Future<void> navigationPage() async {
+    bool isFirstTime = await getPrefrenceBool(isLogin);
+    if (isFirstTime) {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Home(),
+          ));
+    } else {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Login(),
+          ));
+    }
+  }
+
+  setSnackbar(String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(new SnackBar(
+      content: new Text(
+        msg,
+        textAlign: TextAlign.center,
+        style: TextStyle(color: black),
+      ),
+      backgroundColor: white,
+      elevation: 1.0,
+    ));
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.top]);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ));
+    super.dispose();
+  }
+}
